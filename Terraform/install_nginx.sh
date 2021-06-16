a2cd753342f65ff132a6e32cd7bb37eb01e1d5a3
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-06-16 17:50:05 +0200

    update install_nginx.sh

--- Terraform/install_nginx.sh
+++ Terraform/install_nginx.sh
@@ -4,4 +4,5 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-#echo "<h1>DevOps Project One Accomplished Successfully...</h1>" | sudo tee /var/www/html/index.html
+sudo chown ubuntu:ubuntu /var/www/html/ -R
+#sudo rm -f /var/www/html/index.nginx-debian.html
