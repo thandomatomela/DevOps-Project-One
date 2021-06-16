92c1e00e23928ad675ce2a048a3b020286b80d63
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-06-16 17:47:52 +0200

    update .tf and index.html files

--- Terraform/provision-environment.tf
+++ Terraform/provision-environment.tf
@@ -10,15 +10,27 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  key_name = "connect"
   user_data = "${file("install_nginx.sh")}"
-
-  provisioner "copy_website" {
-    source      = "D:\DevOps Projects\DevOps Project One\Demo Website\index.html"
-    destination = "/var/www/html/index.html"
-  }
-  
  
   tags = {
     Name = "Ubuntu Web Server"
-  }
+ }
+}
+
+resource "null_resource" "copy_website"{
+
+    connection {
+        type = "ssh"
+        host = aws_instance.web_server.public_ip
+        user = "ubuntu"
+        private_key = file("../connect.pem")
+    }
+
+    provisioner "file" {
+        source = "D:/DevOps Projects/DevOps Project One/website/"
+        destination = "/var/www/html/"
+    }
+
+    depends_on =  [ aws_instance.web_server ]
 }
