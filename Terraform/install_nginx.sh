9094f5cab1bc30ff144c24bf160c563ce79482c8
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-06-16 13:39:32 +0200

    modified website to contain css

--- Terraform/install_nginx.sh
+++ Terraform/install_nginx.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "<h1>DevOps Project One Accomplished Successfully...</h1>" | sudo tee /var/www/html/index.html
+#echo "<h1>DevOps Project One Accomplished Successfully...</h1>" | sudo tee /var/www/html/index.html
--- Terraform/provision-environment.tf
+++ Terraform/provision-environment.tf
@@ -11,6 +11,12 @@ resource "aws_instance" "web_server" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   user_data = "${file("install_nginx.sh")}"
+
+  provisioner "copy_website" {
+    source      = "D:\DevOps Projects\DevOps Project One\Demo Website\index.html"
+    destination = "/var/www/html/index.html"
+  }
+  
  
   tags = {
     Name = "Ubuntu Web Server"
